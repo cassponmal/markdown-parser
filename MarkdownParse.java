b30ffa922f435eec2b8cfd4e6cd30b330ce61297
alicema1202 <64507532+alicema1202@users.noreply.github.com>
2022-04-15 19:42:50 -0700

    Fixed OutOfMemory error

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -11,15 +11,17 @@ public class MarkdownParse {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
         int currentIndex = 0;
+        //remove all extra characters after last occurrence of ")"
+        markdown = markdown.substring(0, markdown.lastIndexOf(")") + 1); 
         while(currentIndex < markdown.length()) {
             int openBracket = markdown.indexOf("[", currentIndex);
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
             toReturn.add(markdown.substring(openParen + 1, closeParen));
-            currentIndex = closeParen + 1;
+            currentIndex = closeParen + 1;            
         }
-
+        
         return toReturn;
     }
 
@@ -28,6 +30,7 @@ public class MarkdownParse {
         Path fileName = Path.of(args[0]);
         String content = Files.readString(fileName);
         ArrayList<String> links = getLinks(content);
-	    System.out.println(links);
+        System.out.println(links);
+
     }
 }

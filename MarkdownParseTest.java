665705a68eaad0d43bb103dad4ad875612a8a75a
cassponmal <103294645+cassponmal@users.noreply.github.com>
2022-04-22 19:07:32 -0700

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,8 +1,24 @@
 import static org.junit.Assert.*;
 import org.junit.*;
-public class MarkdownParseTest {
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MarkdownParseTest  {
+    
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+
+    @Test
+    public void MarkdownParseTest() throws IOException {
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);  
+
+        assertEquals(MarkdownParse.getLinks(content), List.of("https://something.com", "some-thing.html"));
     }
 }

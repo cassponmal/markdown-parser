95c98bc716edce5e56d2cf5545f875f46ecc4250
cassponmal <103294645+cassponmal@users.noreply.github.com>
2022-04-24 18:21:09 -0700

    Solution to Image Reference Problem

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -18,7 +18,7 @@ public class MarkdownParse {
             int closeBracket = markdown.indexOf("]", openBracket);
             int openParen = markdown.indexOf("(", closeBracket);
             int closeParen = markdown.indexOf(")", openParen);
-            if( closeBracket + 1 == openParen){
+            if( closeBracket + 1 == openParen  && (openBracket-1 != markdown.indexOf("!"))){
                 toReturn.add(markdown.substring(openParen + 1, closeParen));
             }
             currentIndex = closeParen + 1;            
